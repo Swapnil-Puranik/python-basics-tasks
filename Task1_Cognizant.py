bc4e48ecd2e1f5f2b8384f807ce3c90f494d42c9
Swapnil-Puranik <puran037@umn.edu>
2025-05-15 21:38:53 -0500

    Add files via upload

---
+++ Task1_Cognizant.py
@@ -0,0 +1,40 @@
+#Task 1
+name = "Swapnil"
+age = 26
+height = 5.7
+
+# Printing the description
+print("Hey there, my name is " + name +"! I’m " + str(age) +" years old and " + str(height) + " feet tall. Glad to meet you!!")
+
+#Task 2
+num1 = 12
+num2 = 5
+
+# Addition
+print("Addition:", num1 + num2)
+
+# Subtraction
+print("Subtraction:",num1 - num2)
+
+# Multiplication
+print("Multiplication: ", num1 * num2)
+
+# Division
+print("Division: ", num1 / num2)
+
+# Task 3
+number = float(input("Enter any number, and I’ll tell you what kind of vibe it gives off:"))
+
+# Conditional logic to check if the number is positive, negative, or zero
+if number > 0:
+    print("This number is positive. Awesome!")
+elif number < 0:
+    print("This number is negative. Better luck next time!")
+else:
+    print("Zero it is. A perfect balance!")
+
+#Excited to be a part of Cognizant!!!!
+
+
+
+
